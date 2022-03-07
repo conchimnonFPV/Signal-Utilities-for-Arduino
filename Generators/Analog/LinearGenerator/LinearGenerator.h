2555b0b3899ca8a9bc267b4f61f08f224db7e318
Tai-Min <22417805+Tai-Min@users.noreply.github.com>
2022-03-07 21:13:17 +0100

    Update LinearGenerator.h

--- Generators/Analog/LinearGenerator/LinearGenerator.h
+++ Generators/Analog/LinearGenerator/LinearGenerator.h
@@ -13,7 +13,7 @@ namespace Analog
 /**
  * \brief Generator that generates linear signal from one value to another in given time.
  */
-class LinearGenerator : SigUtil::Generators::Analog::AbstractAnalogGenerator
+class LinearGenerator : public SigUtil::Generators::Analog::AbstractAnalogGenerator
 {
 private:
     double a, b;
